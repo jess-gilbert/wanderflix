239782587e0d924f4a25bd3a5e06f273e5dc9440
Careyyy <careyyuen1026@gmail.com>
2022-10-16 16:59:29 +0100

    SearchBar & button on the Home page

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,10 +1,13 @@
 import './Home.css';
+import SearchBar from '../searchBar';
 
 export default function Home() {
     return (
         <>
             <div className="Wallpaper">
                 <p></p>
+                <p className='Title'> MOVIE AND TV RECOMMENDATIONS SO YOU CAN TRAVEL THE WORLD WITHOUT LEAVING YOUR COUCH</p>
+                <SearchBar/>
             </div>
         </>
     )
