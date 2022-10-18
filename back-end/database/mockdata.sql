5c43e6e097a6296ba844870dea951e45a2ec8fd9
ozge-ozersahin <ozgedonmez39@gmail.com>
2022-10-18 14:56:46 +0100

    sign-in and sign-up APIs has been created. Postman folders created.

--- back-end/database/mockdata.sql
+++ back-end/database/mockdata.sql
@@ -1,29 +1,23 @@
-INSERT INTO wanderflix.users (user_id, user_email, user_password) 
-VALUES ('1', 'ozge@gmail.com' , '1234'),
-		('2', 'carey@gmail.com' , '4567'),
-        ('3', 'nicola@gmail.com' , '5678'),
-        ('4', 'jess@gmail.com', '1289'),
-        ('5', 'daniela@gmail.com', '3456');
+INSERT INTO wanderflix.users (user_email, user_password,first_name, last_name) 
+VALUES ( 'ozge@gmail.com' , '1234', 'Ozge', 'Ozersahin'),
+		( 'carey@gmail.com' , '4567', 'Carey', 'Yuen'),
+        ( 'nicola@gmail.com' , '5678', 'Nicola', 'Ward'),
+        ( 'jess@gmail.com', '1289','Jess', 'Gilbert'),
+        ( 'daniela@gmail.com', '3456', 'Daniela', 'M');
      
-INSERT INTO wanderflix.user_profile (user_id,first_name, last_name, user_language)
-VALUES ('1','Ozge','Ozersahin', 'ENG'),
-		('2','Carey','Yuen','ENG'),
-        ('3','Nicola','Ward', 'ENG'),
-        ('4','Jess','Gilbert', 'ENG'),
-        ('5','Daniela','M', 'ENG');
+     
         
-
+ INSERT INTO wanderflix.user_watchlist (user_id, movie_id)
+ VALUES ('16','tt0944947'),
+		('17','tt0944947'),
+        ('18','tt0944947'),
+        ('19','tt0944947'),
+        ('20','tt0944947');
         
- INSERT INTO wanderflix.user_watchlist (user_id, movie_id, watchlist_id)
- VALUES ('1','tt0944947','10'),
-		('2','tt0944947','11'),
-        ('3','tt0944947','12'),
-        ('4','tt0944947','13'),
-        ('5','tt0944947','14');
         
- INSERT INTO wanderflix.user_review ( user_id , movie_id, review_id, star_rating, user_comment)
- VALUES ('1','tt0944947','abc','4.0','I like the scenario'),
-		('2','tt0944947','xyz','4.1','I love this actress'),
-        ('3','tt0944947','def','4.5','Way better than I expected'),
-        ('4','tt0944947', 'dlm','4.2','Nice work'),
-        ('5','tt0944947','klm','4.3.','Love the movie');
+ INSERT INTO wanderflix.user_review ( user_id , movie_id, star_rating, user_comment)
+ VALUES ('16','tt0944947','4.0','I like the scenario'),
+		('17','tt0944947','4.1','I love this actress'),
+        ('18','tt0944947','4.5','Way better than I expected'),
+        ('19','tt0944947','4.2','Nice work'),
+        ('20','tt0944947','4.3.','Love the movie');
--- back-end/database/movie_db.sql
+++ back-end/database/movie_db.sql
@@ -5,40 +5,31 @@ CREATE DATABASE wanderflix;
 USE wanderflix;
 
 CREATE TABLE `users` (
-  `user_id` varchar(15) NOT NULL,
+  `user_id` int NOT NULL AUTO_INCREMENT,
   `user_email` varchar(100) NOT NULL,
   `user_password` varchar(15) NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_email_UNIQUE` (`user_email`),
   UNIQUE KEY `user_id_UNIQUE` (`user_id`)
 );
 
-
-
-CREATE TABLE `user_profile` (
-  `user_id` varchar(15) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `user_language` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
-) ;
-    
-
  
 CREATE TABLE `user_watchlist` (
-  `user_id` varchar(15) NOT NULL,
+  `user_id` int NOT NULL,
   `movie_id` varchar(50) NOT NULL,
-  `watchlist_id` varchar(15) NOT NULL,
+  `watchlist_id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`watchlist_id`),
   UNIQUE KEY `user_id_UNIQUE` (`user_id`),
   UNIQUE KEY `watchlist_id_UNIQUE` (`watchlist_id`)
 );
 
+
 CREATE TABLE `user_review` (
-  `user_id` varchar(15) NOT NULL,
+  `user_id` int NOT NULL,
   `movie_id` varchar(15) NOT NULL,
-  `review_id` varchar(15) NOT NULL,
+  `review_id` int NOT NULL AUTO_INCREMENT,
   `star_rating` varchar(5) DEFAULT NULL,
   `user_comment` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`review_id`),
