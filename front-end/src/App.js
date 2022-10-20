48b6a5b38c6521db7dd6560cf93f7d4c0674a4be
jess-gilbert <105994412+jess-gilbert@users.noreply.github.com>
2022-10-20 19:26:43 +0100

    results page working

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,8 +6,7 @@ import ResultPage from "./pages/ResultPage";
 import Watchlist from "./pages/Watchlist";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/SignUp";
-
-import {GlobalProvider} from "./context/GlobalState";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
@@ -19,7 +18,7 @@ function App() {
         <Route path="/Home" element={<Home />} />
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/ResultPage" element={<ResultPage />} />
+        <Route path="/ResultPage/:query" element={<ResultPage />} />
         <Route path="/Watchlist" element={<Watchlist />} />
       </Routes>
     </div>
