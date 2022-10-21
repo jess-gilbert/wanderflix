0ac75fa18a8f2a9b88d7438b09ae934177fdb3e9
jess-gilbert <105994412+jess-gilbert@users.noreply.github.com>
2022-10-21 16:58:38 +0100

    Fixed movie card context errors

--- front-end/src/movieCard/movieCard.js
+++ front-end/src/movieCard/movieCard.js
@@ -1,34 +1,40 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import "./movieCard.css";
+import { addMovieToWatchlist } from "../context/AppActions";
 
-export default function MovieCard({movie}){
-    const {addMovieToWatchlist, watchlist} = useContext(GlobalContext);
+export default function MovieCard({ movie }) {
+  const [contextState, dispatch] = useContext(GlobalContext);
 
-    let storedMovie = watchlist.find( o => o.di === movie.id);
+  let storedMovie = contextState.watchlist.find((o) => o.di === movie.id);
 
-    const watchlistDisabled = storedMovie ? true : false;
+  const watchlistDisabled = storedMovie ? true : false;
 
-    return (
+  return (
     <div className="card">
-<div 
-    className="controls">
-        <button className="btn" 
-        disabled={watchlistDisabled}
-        onClick={() => addMovieToWatchlist (movie)}>
-            <i class="fa-sharp fa-solid fa-heart"></i> SAVE TO WATCHLIST</button>
+      <div className="controls">
+        <button
+          className="btn"
+          disabled={watchlistDisabled}
+          onClick={() => dispatch(addMovieToWatchlist(movie))}
+        >
+          <i class="fa-sharp fa-solid fa-heart"></i> SAVE TO WATCHLIST
+        </button>
 
-       <img className="card--image"
-           src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-           alt={movie.title + 'poster'}
-           />
-       <div className="card--content">
-       <h3 className="caed--title">{movie.title}</h3>
-       {/* <p><small>RELEASE DATE: {movie.release_date}</small></p> */}
-       <p><small>RATING: {movie.vote_average}</small></p>
-       {/* <p className="card--desc">{movie.overview}</p> */}
-       </div>
-       </div>
+        <img
+          className="card--image"
+          src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+          alt={movie.title + "poster"}
+        />
+        <div className="card--content">
+          <h3 className="caed--title">{movie.title}</h3>
+          {/* <p><small>RELEASE DATE: {movie.release_date}</small></p> */}
+          <p>
+            <small>RATING: {movie.vote_average}</small>
+          </p>
+          {/* <p className="card--desc">{movie.overview}</p> */}
+        </div>
+      </div>
     </div>
-    )
+  );
 }
