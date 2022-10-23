26510910f9bc6f1472019712ab09e12b3e24e0ba
jess-gilbert <105994412+jess-gilbert@users.noreply.github.com>
2022-10-23 22:01:55 +0100

    added remove from watchlist and adjusted icons

--- front-end/src/context/GlobalState.js
+++ front-end/src/context/GlobalState.js
@@ -24,6 +24,9 @@ export const GlobalProvider = (props) => {
     addMovieToWatchlist: (payload) => {
       dispatch({ type: "ADD_MOVIE_TO_WATCHLIST", payload: payload });
     },
+    removeMovieFromWatchlist: (payload) => {
+      dispatch({ type: "REMOVE_MOVIE_FROM_WATCHLIST", payload: payload });
+    },
     setUserSignedIn: (payload) => {
       dispatch({ type: "SET_USER_SIGNED_IN", payload: payload });
     },
