1ddc591089e035bd944a26df8be2d4b247a5d443
Careyyy <careyyuen1026@gmail.com>
2022-10-24 20:58:51 +0100

    Movie info card overlay button

--- front-end/src/pages/Watchlist.js
+++ front-end/src/pages/Watchlist.js
@@ -1,12 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../context/GlobalState";
 import MovieCard from "../movieCard/movieCard";
 import "./ResultPage.css";
+import { Axios } from "axios";
 
 export default function Watchlist() {
   const { watchlist } = useContext(GlobalContext);
+  // const [movie_id, setMovie_id] = useState("");
+  // const [user_id, setUser_id]  = useState ("");
 
-  return (
+// useEffect (() => {
+//   Axios.get("http://127.0.0.1:4000/watchlist/api/insert").then((Response) => {
+//     console.log (Response.data);
+//   });
+// }, []);
+
+
+//   const saveToWatchlist = () => {
+//       Axios. post ("http://127.0.0.1:4000/watchlist", {
+//         movie_id : movie_id,
+//         user_id : user_id,
+//       }).then (() => { 
+//         alert("Successful insert");
+//       });
+//       };
+   
+
+return (
     <div className="center-max-size">
       <div className="card-list">
         {watchlist
@@ -17,4 +37,4 @@ export default function Watchlist() {
       </div>
     </div>
   );
-}
+  };
