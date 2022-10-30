ca7428e43268752e1d70eb65148dc98103dbc941
Nicola Ward <nicolaogle@ymail.com>
2022-10-30 10:27:38 +0000

    Discover Pages/HomePage change

--- front-end/src/pages/Discover.js
+++ front-end/src/pages/Discover.js
@@ -1,42 +1,74 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Discover.css";
 import RomeSkyline from "../images/RomeSkyline.png";
 import ParisSkyline from "../images/ParisSkyline.png";
 import LondonSkyline from "../images/LondonSkyline.png";
 import SingaporeSkyline from "../images/SingaporeSkyline.png";
 
+const cities = [
+  {
+    city: "London",
+    city_id: 212,
+    class: "london-skyline",
+    src: LondonSkyline,
+    alt: "London Sky Line",
+  },
+
+  {
+    city: "Paris",
+    city_id: 90,
+    class: "paris-skyline",
+    src: ParisSkyline,
+    alt: "Paris Sky Line",
+
+  },
+
+  {
+    city: "Rome",
+    city_id: 588,
+    class: "rome-skyline",
+    src: RomeSkyline,
+    alt: "Rome Sky Line",
+  },
+
+  {
+    city: "Singapore",
+    city_id: 4224,
+    class: "singapore-skyline",
+    src: SingaporeSkyline,
+    alt: "Singapore Sky Line",
+  },
+];
+
 export default function Discover() {
+  const navigate = useNavigate();
   return (
     <>
-     <h1 className="discover-title">Explore your next destination...</h1>
+      <h1 className="discover-title">Explore your next destination...</h1>
       <h2 className="sub-title">
-          Not sure where to head next? Discover curated lists of movies from cities around the globe.
+        Not sure where to head next? Discover curated lists of movies from
+        cities around the globe.
       </h2>
+      <p className="p1">
+      Click through to explore some of our favourite destinations well served by cinema, from the 
+      stunning historic back-drops of Rome to the romantic scenery of Paris. </p>
+
+      <p className="p2">Not all of these movies are masterpieces (although there are a few that 
+      are) each of them effectively immerses you in the culture of the destination.
+      </p>
       <div className="grid-container">
-        <div className="grid-item">
-          <img className = "london-skyline" src={LondonSkyline} alt="London Sky Line" />
-          <h3 class="box-title-1">
-            LONDON
-        </h3>
-        </div>
-        <div className="grid-item">
-          <img className = "rome-skyline" src={RomeSkyline} alt="Rome Sky Line" />
-          <h3 class="box-title-2">
-            ROME
-        </h3>
-        </div>
-        <div className="grid-item">
-          <img className = "paris-skyline" src={ParisSkyline} alt="Paris Sky Line" />
-          <h3 class="box-title-3">
-            PARIS
-          </h3>
-        </div>
-        <div className="grid-item4">
-          <img className = "singapore-skyline" src={SingaporeSkyline} alt="Singapore Sky Line" />
-          <h3 class="box-title-4">
-            SINGAPORE
-        </h3>
-        </div>
+        {cities.map((city) => (
+          <div className="grid-item">
+            <img
+              className={city.class}
+              src={city.src}
+              alt={city.alt}
+              onClick={() => navigate(`/DiscoverResults/${city.city_id}`)}
+            />
+          </div>
+        ))}
       </div>
     </>
-  )
+  );
 }
